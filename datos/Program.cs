98f1d29a10dd0bf8ba58447013d0d2c29d52f10d
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2023-11-09 01:17:54 -0300

    SqlServer connect C# initial commit

---
+++ datos/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using DotNetEnv;
+
+namespace Datos
+{
+  class Program
+  {
+      static void Main(string[] args)
+      {
+        DotNetEnv.Env.Load();
+        D_Clientes dClientes = new D_Clientes();
+        DataTable tablaClientes = dClientes.Mostrar();
+
+        foreach (DataRow row in tablaClientes.Rows)
+        {
+          Console.WriteLine($"{row["NOMBRE"]} - {row["APELLIDO"]}");
+        }
+
+        Console.ReadLine();
+      }
+    }
+  }
+
+  public class D_Clientes {
+  private DB_Conexion conexion = new DB_Conexion();
+  SqlDataReader reader;
+  DataTable tabla = new DataTable();
+  SqlCommand query = new SqlCommand();
+
+  public DataTable Mostrar() {
+    try
+    {
+        query.Connection = conexion.AbrirConexion();
+        query.CommandText = "SELECT * FROM CLIENTES";
+        reader = query.ExecuteReader();
+        tabla.Load(reader);
+        return tabla;
+    }
+    finally
+    {
+        conexion.CerrarConexion();
+    }
+  }
+  
+  public class DB_Conexion
+  {
+    string db_server = Environment.GetEnvironmentVariable("db_server");
+    private SqlConnection Conexion;
+
+    public DB_Conexion()
+    {
+        Conexion = new SqlConnection($"Server={db_server};DataBase=Final;Integrated Security=true");
+    }
+    
+    public SqlConnection AbrirConexion()
+    {
+      if (Conexion.State == ConnectionState.Closed) { Conexion.Open(); Console.WriteLine("db is connected"); }
+      return Conexion;
+    }
+
+    public SqlConnection CerrarConexion()
+    {
+      if (Conexion.State == ConnectionState.Open) { Conexion.Close(); }
+      return Conexion;
+    }
+
+  }
+}
